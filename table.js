ade1826b3f9399d2e6b72edbfc2ec0ac8556abc1
Charlie <106720398+Chrarlie1@users.noreply.github.com>
2024-11-21 08:33:45 +0800

    Add files via upload

--- table.js
+++ table.js
@@ -3,5 +3,5 @@ const db = new sqlite.Database("./qoute.db", sqlite.OPEN_READWRITE, (err)=>{
     if (err) return console.error(err);
 });
 
-const sql = 'CREATE TABLE quote (ID INTEGER PRIMARY KET, movie, quote, character)';
+const sql = 'CREATE TABLE quote (ID INTEGER PRIMARY KEY, movie STRING, quote STRING, character STRING)';
 db.run(sql);
